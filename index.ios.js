7036f511ece081324e7c4230f32040b840d640b1
Belde, Vikram <belvikram@gmail.com>
2016-10-16 03:42:53 -0400

    Layout Fix

--- index.ios.js
+++ index.ios.js
@@ -19,16 +19,17 @@ export default class Franklin extends Component {
    }
 
   render() {
+     var randomIndex = this.getRandomInt(0,12)
     return (
       <View style={{flex:1,backgroundColor:'#FF5E3A',alignItems:'center',justifyContent:'center'}}>
             <View style={{flex:0.5,alignItems:'center',justifyContent:'center'}}>
-               <Text style={{fontSize:30,fontFamily:'AvenirNext-Bold',color:'#FFFFFF',textAlign:'center'}}> {facts[this.getRandomInt(0,13)].virtue}</Text>
+               <Text style={{fontSize:30,fontFamily:'AvenirNext-Bold',color:'#FFFFFF',textAlign:'center'}}> {facts[randomIndex].virtue}</Text>
             </View>
-            <View style={{flex:2,marginLeft:50,marginRight:50,marginBottom:50,alignItems:'center',justifyContent:'center',backgroundColor:'transparent',opacity:1}}>
-               <Text style={{fontSize:30,fontFamily:'AvenirNext-Bold',color:'#FFFFFF',textAlign:'center'}}> {facts[this.getRandomInt(0,13)].detail}</Text>
+            <View style={{flex:1,marginLeft:50,marginRight:50,marginBottom:50,alignItems:'center',justifyContent:'center',backgroundColor:'transparent',opacity:1}}>
+               <Text style={{fontSize:25,fontFamily:'AvenirNext-Bold',color:'#FFFFFF',textAlign:'center'}}> {facts[randomIndex].detail}</Text>
             </View>
-            <View style={{flex:1,marginBottom:50}}>
-               <TouchableHighlight underlayColor= '#EEEEEE' onPress = {()=>{this.setState({random:Math.random()})}}  style={{height:100,width:200,backgroundColor:'#FFFFFF',opacity:0.8,alignItems:'center',justifyContent:'center'}}>
+            <View style={{flex:0.5}}>
+               <TouchableHighlight underlayColor= '#EEEEEE' onPress = {()=>{this.setState({random:Math.random()})}}  style={{height:60,width:200,backgroundColor:'#FFFFFF',opacity:0.8,alignItems:'center',justifyContent:'center'}}>
                      <Text style={{fontSize:20,fontFamily:'AvenirNext-Bold',textAlign:'center'}}>Next Virtue</Text>
                </TouchableHighlight>
             </View>
